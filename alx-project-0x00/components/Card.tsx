4c01d7af4499cab9c29dc96ded5009b9728d105d
DevMandate <wafulacaleb233@gmail.com>
2025-07-18 19:17:30 +0300

    Advanced Task Done

--- alx-project-0x00/components/Card.tsx
+++ alx-project-0x00/components/Card.tsx
@@ -11,7 +11,7 @@ const Card: React.FC = () => {
 
  return (
     <div className="h-[422px] w-[378.56px] cursor-pointer hover:shadow-md hover:rounded-lg ">
-      <Image className="rounded-lg" src={HOUSE_IMAGE} width={378.56} height={299.37} alt="house image" />
+      <Image className="rounded-lg" src={HOUSE_IMAGE} width={350.56} height={250.37} alt="house image" />
       <div className="p-2 flex gap-2 mt-2">
         <Pill title="Top Villa" />
         <Pill title="Self CheckIn" />
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -13,6 +13,7 @@ const Landing: React.FC =  () => {
         <Button title="Small Rounded" styles="text-sm px-4 py-1 rounded-sm bg-blue-500" />
         <Button title="Medium Rounded-md" styles="text-base px-5 py-2 rounded-md bg-green-500" />
         <Button title="Large Rounded-full" styles="text-lg px-6 py-3 rounded-full bg-red-500" />
+        <Button title="Large Rounded-lg" styles="text-base px-5 py-2 rounded-lg bg-purple-500" />
       </div>
 
     </div>
